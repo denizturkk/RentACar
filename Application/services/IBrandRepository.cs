1ee3b0ec04e83f960e60b2ebc7d0ca8209746547
Deniz Türk <deniz.turk@ogr.gsu.edu.tr>
2023-11-17 16:47:17 +0300

    Add project files.

---
+++ Application/services/IBrandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Repositories;
+using Core.Persistence.Repository;
+using Domain.Entitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.services;
+
+public interface IBrandRepository : IAsyncRepository<Brand, Guid>, IRepository<Brand,Guid>
+{
+}
---
+++ Domain/Entitities/Brand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repository;
+
+
+
+namespace Domain.Entitities;
+
+public class Brand : Entity<Guid>
+{
+    public string Name { get; set; }
+    public Guid Id { get; set; } 
+
+    public Brand()
+    {
+            
+    }
+    public Brand(Guid id ,string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+}
